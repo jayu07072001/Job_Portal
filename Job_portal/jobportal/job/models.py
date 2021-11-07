601208fffb618c4437d10c82ef06eee105eff735
jayesh <jayeshlipare2001@gmail.com>
2021-11-07 18:11:50 +0530

    signup user data added

--- Job_portal/jobportal/job/models.py
+++ Job_portal/jobportal/job/models.py
@@ -3,9 +3,7 @@ from django.contrib.auth.models import User
 
 class job_seeker(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    mobile=models.IntegerField(max_length=10)
-    file=models.FileField()
+    mobile=models.CharField(max_length=10)
     gender=models.CharField(max_length=20,null=True)
-
     def __str__(self):
         return self.user.username
--- Job_portal/jobportal/job/views.py
+++ Job_portal/jobportal/job/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from .models import *
 
 def home(request):
     return render(request, 'job/home.html')
@@ -16,7 +18,24 @@ def recruiter_login(request):
     return render(request,'job/recruiter_login.html')
 
 def signup_jobseeker(request):
-    return render(request,'job/signup_jobseeker.html')
+    error=""
+    if request.method=='POST':
+        f=request.POST['fname']
+        l=request.POST['lname']
+        e=request.POST['email']
+        con=request.POST['cont']
+        p=request.POST['pwd1']
+        gen=request.POST['gender']
+        try:
+           user= User.objects.create_user(first_name=f,last_name=l,username=e,password=p)
+           job_seeker.objects.create(user=user,mobile=con,gender=gen)
+           error="no"
+        except:
+            error="yes"
+        
+    d={'error':error}
+    
+    return render(request,'job/signup_jobseeker.html',d)
 
 def signup_recruiter(request):
     return render(request,'job/signup_recruiter.html')
