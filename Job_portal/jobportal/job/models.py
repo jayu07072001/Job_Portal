8cfbfc5dde213c12b6449bcf4fcc8a5b84d40ec4
jayesh <jayeshlipare2001@gmail.com>
2021-11-08 20:10:29 +0530

    Successfuly login jobseeker

--- Job_portal/jobportal/job/models.py
+++ Job_portal/jobportal/job/models.py
@@ -5,5 +5,16 @@ class job_seeker(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     mobile=models.CharField(max_length=10)
     gender=models.CharField(max_length=20,null=True)
+    type=models.CharField(max_length=20,null=True)
+    def __str__(self):
+        return self.user.username
+
+class recruiter(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    mobile=models.CharField(max_length=10)
+    gender=models.CharField(max_length=20,null=True)
+    company=models.CharField(max_length=50)
+    type=models.CharField(max_length=20,null=True)
+    status=models.CharField(max_length=20,null=True)
     def __str__(self):
         return self.user.username
--- Job_portal/jobportal/job/views.py
+++ Job_portal/jobportal/job/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
+from django.contrib.auth import authenticate,login,logout
 
 def home(request):
     return render(request, 'job/home.html')
@@ -11,11 +12,37 @@ def contact(request):
 def about(request):
     return render(request,'job/about.html')
 
-def user_login(request):
-    return render(request,'job/user_login.html')
+def user_about(request):
+    return render(request,'job/user_about.html')
 
-def recruiter_login(request):
-    return render(request,'job/recruiter_login.html')
+def user_contact(request):
+    return render(request,'job/user_contact.html')
+
+def user_login(request):
+    error=""
+    if request.method=='POST':
+        u=request.POST['uname']
+        p=request.POST['pwd3']
+        user=authenticate(username=u,password=p)
+        if user:
+            try:
+                user1 = job_seeker.objects.get(user=user)
+                if user1.type == "jobseeker":
+                    login(request,user)
+                    error="no"
+                else:
+                    error="yes"
+            except:
+                error="yes"
+        else:
+            error="yes"
+        
+    d={'error':error}
+
+
+
+    return render(request,'job/user_login.html',d)
+
 
 def signup_jobseeker(request):
     error=""
@@ -26,9 +53,10 @@ def signup_jobseeker(request):
         con=request.POST['cont']
         p=request.POST['pwd1']
         gen=request.POST['gender']
+        type="jobseeker"
         try:
            user= User.objects.create_user(first_name=f,last_name=l,username=e,password=p)
-           job_seeker.objects.create(user=user,mobile=con,gender=gen)
+           job_seeker.objects.create(user=user,mobile=con,gender=gen,type=type)
            error="no"
         except:
             error="yes"
@@ -37,5 +65,38 @@ def signup_jobseeker(request):
     
     return render(request,'job/signup_jobseeker.html',d)
 
+def user_home(request):
+    if not request.user.is_authenticated:
+        return redirect('user_login')
+
+    return render(request,'job/user_home.html')
+
+
+def Logout(request):
+    logout(request)
+    return redirect('home')
+
+
 def signup_recruiter(request):
-    return render(request,'job/signup_recruiter.html')
+    error=""
+    if request.method=='POST':
+        f=request.POST['fname']
+        l=request.POST['lname']
+        e=request.POST['email']
+        c=request.POST['company']
+        con=request.POST['cont']
+        p=request.POST['pwd1']
+        type="recruiter"
+        try:
+           user= User.objects.create_user(first_name=f,last_name=l,username=e,password=p)
+           job_seeker.objects.create(user=user,mobile=con,type=type,company=c,status="pending")
+           error="no"
+        except:
+            error="yes"
+        
+    d={'error':error}
+    
+    return render(request,'job/signup_recruiter.html',d)
+
+def recruiter_login(request):
+    return render(request,'job/recruiter_login.html')
